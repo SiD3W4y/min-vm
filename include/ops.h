c5756d61ef956ed486fa6b8cae91d8180647e654
Sideway <dev.sideway@gmail.com>
2017-07-12 20:36:11 +0200

    Changed ops indexing

--- include/ops.h
+++ include/ops.h
@@ -1,16 +1,16 @@
 #ifndef OPS_H
 #define OPS_H
 
-#define OP_SYS = 0x00
-#define OP_ADD = 0x01
-#define OP_SUB = 0x02
-#define OP_MUL = 0x03
-#define OP_MOV = 0x04
-#define OP_LDR = 0x05
-#define OP_PUSH = 0x06
-#define OP_POP = 0x07
-#define OP_CMP = 0x08
-#define OP_JMP = 0x09
+#define OP_SYS 0x01
+#define OP_ADD 0x02
+#define OP_SUB 0x03
+#define OP_MUL 0x04
+#define OP_MOV 0x05
+#define OP_LDR 0x06
+#define OP_PUSH 0x07
+#define OP_POP 0x08
+#define OP_CMP 0x09
+#define OP_JMP 0x0a
 
 #include <stdbool.h>
 
@@ -22,6 +22,5 @@ typedef struct vm_opcode{
 	int second_value;
 }vm_opcode;
 
-char *OPS_STR[] = {"SYS","ADD","SUB","MUL","MOV","LDR","PUSH","POP","CMP","JMP"};
 
 #endif
