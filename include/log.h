4412b78b9276967b0798766dcc28a94fa37cea5e
Sideway <dev.sideway@gmail.com>
2017-07-13 21:05:09 +0200

    Added minimum log pretty printing support

---
+++ include/log.h
@@ -0,0 +1,8 @@
+#ifndef LOG_H
+#define LOG_H
+
+void log_info(char *fmt,...);
+void log_warn(char *fmt,...);
+void log_error(char *fmt,...);
+
+#endif
---
+++ src/log.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+#include "log.h"
+
+static void log_raw(char* title,char* color,char *fmt,va_list args)
+{
+	printf("%s%s%s - ",color,title,"\x1b[0m");
+	vprintf(fmt,args);
+}
+
+void log_info(char *fmt,...)
+{
+	va_list args;
+	va_start(args,fmt);
+	log_raw("INFO","\x1b[32m",fmt,args);
+}
+
+void log_warn(char *fmt,...)
+{
+	va_list args;
+	va_start(args,fmt);
+	log_raw("WARNING","\x1b[33m",fmt,args);
+}
+
+void log_error(char *fmt,...)
+{
+	va_list args;
+	va_start(args,fmt);
+	log_raw("ERROR","\x1b[31m",fmt,args);
+}
