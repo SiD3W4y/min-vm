3eb0a007311f00840ba9116fc3cc279451d056f6
Sideway <dev.sideway@gmail.com>
2017-12-06 08:58:46 +0100

    Added dumb linked list implementation

---
+++ include/utils/list.h
@@ -0,0 +1,22 @@
+#ifndef LIST_H
+#define LIST_H
+
+typedef struct node_t {
+	void *data;
+	struct node *next;
+	struct node *prev;
+}node;
+
+typedef struct list_t {
+	node *head;
+	node *tail;
+}list;
+
+node *node_new(void *data);
+
+list *list_new();
+void list_append(list *lst,node *elem);
+void list_pop(list *lst);
+void list_free(list *lst);
+
+#endif
---
+++ src/utils/list.c
@@ -0,0 +1,75 @@
+#include <stdlib.h>
+#include <stdint.h>
+
+#include "utils/list.h"
+
+node *node_new(void *data)
+{
+	node *n = malloc(sizeof(node));
+
+	n->data = data;
+	n->prev = NULL;
+	n->next = NULL;
+
+	return n;
+}
+
+list *list_new()
+{
+	list *l = malloc(sizeof(list));
+	l->head = NULL;
+	l->tail = NULL;
+
+	return l;
+}
+
+void list_append(list *lst,node *elem)
+{
+	if(lst->head != NULL){
+		node *old_tail = lst->tail;
+
+		old_tail->next = elem;
+		elem->prev = old_tail;
+
+		lst->tail = elem;
+	}
+
+	if(lst->head == NULL && lst->tail == NULL){
+		lst->head = elem;
+		lst->tail = elem;
+	}
+
+}
+
+void list_pop(list *lst)
+{
+	node *tail = lst->tail;
+
+	if(lst->head != NULL && lst->tail != NULL){
+		if(tail == lst->head){
+			lst->head = NULL;
+			lst->tail = NULL;
+		}else{
+			lst->tail = tail->prev;
+			lst->tail->next = NULL;
+		}
+
+		free(tail->data);
+		free(tail);
+	}
+}
+
+void list_free(list *lst)
+{
+	node *n = lst->head;
+	node *c;
+
+	while(n->next != NULL){
+		c = n->next;
+		free(n->data);
+		free(n);
+		n = c;
+	}
+
+	free(lst);
+}
